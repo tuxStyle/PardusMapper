d00793f93912577a3ba518865f759e53652a0869
Catalin C <tuxstyle@gmail.com>
2024-10-12 01:35:46 +0000

    - update user script version

--- templates/footer.php
+++ templates/footer.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 /** @var string $base_url */
 /** @var string $uni */
 ?>
-<div id="6.10"><h3><a href="<?php echo $base_url . '/' . $uni; ?>/download/?dl=1">Download<br />User Script<br /> v 6.10</a></h3></div>
+<div id="6.11"><h3><a href="<?php echo $base_url . '/' . $uni; ?>/download/?dl=1">Download<br />User Script<br /> v 6.11</a></h3></div>
 <div id="paypal">
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick" />
